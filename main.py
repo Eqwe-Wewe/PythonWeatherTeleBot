81f3cb0693ece6f06daa415cd9a8582b19d4ad9b
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-07-06 02:55:43 +0600

    Update main.py
    improved selection of regions

--- main.py
+++ main.py
@@ -46,27 +46,6 @@ def current_weather(message):
             callback_data='update_current'))
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def weather_callback(query):
-    data = query.data
-    bot.answer_callback_query(query.id)
-    bot.send_chat_action(query.message.chat.id, 'typing')
-    if data == 'update_current':
-        bot.send_message(
-            query.message.chat.id,
-            get_message(),
-            reply_markup=button(
-                text='Обновить',
-                callback_data='update_current'))
-    elif data == 'update_10_days':
-        bot.send_message(
-            query.message.chat.id,
-            get_message_10_days(),
-            reply_markup=button(
-                text='Обновить',
-                callback_data='update_10_days'))
-
-
 def button(text: str, url: str = None, callback_data: str = None):
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.add(
@@ -84,15 +63,19 @@ def get_message():
     weather_value = parsing(
         'div',
         'term__value')
-    return (f'{city[0]}\n'+
-            f'текущая температура {"".join([weather_value[0], "C°"])},\n' +
+    condition = parsing(
+        'div',
+        'link__condition day-anchor i-bem')
+    return (f'{city[0]} \U0001F609\n' +
+            f'текущая температура {"".join([weather_value[0], "C°"])}\n' +
             f'ощущается как {"".join([weather_value[1], "C°"])}\n' +
+            f'{condition[0].lower()}\n' +
             f'ветер {weather_value[2]}\n' +
             f'влажность {weather_value[3]}\n' +
             f'давление {weather_value[4]}')
 
 
-def parsing(name: str, attrs: str):
+def parsing(name: str, attrs: str, url: str = url):
     html = requests.get(url)
     soup = BeautifulSoup(html.text, 'html.parser')
     value = soup.findAll(name, class_=attrs)
@@ -130,41 +113,81 @@ def get_message_10_days():
     condition = parsing(
         'div',
         'forecast-briefly__condition')
-    # img = parsing(
-    #    'img',
-    #    'class="icon icon_thumb_skc-d icon_size_48 icon_color_dark forecast-briefly__icon')
-    mes =[', '.join([ten_day[i],
-                     time[i],
-                     t_day[i],
-                     t_night[i],
-                     condition[i]])
-          + '\n\n'
-          for i in range(2, 12)]
-    return  (city[0]
-             + '\n'
-             + 'Прогноз на 10 дней'
-             + '\n\n'
-             + ''.join(mes))
+    mes = [', '.join([ten_day[i],
+                      time[i],
+                      t_day[i],
+                      t_night[i],
+                      condition[i]]) + '\n\n'
+           for i in range(2, 12)]
+    return (city[0]
+            + '\n'
+            + 'Прогноз на 10 дней'
+            + '\n\n'
+            + ''.join(mes))
 
 
 @bot.message_handler(commands=['location_selection'])
 def location_selection(message):
     bot.send_chat_action(message.chat.id, 'typing')
-
-    # example
-    keyboard = telebot.types.InlineKeyboardMarkup(row_width=5)
-    lst=[]
-    for i in range(19):
-        s = telebot.types.InlineKeyboardButton(
-            'Обновить',
-            callback_data='update_10_days')
-        lst.append(s)
+    alphabet = parsing(
+        'h2',
+        'title title_level_2 place-list__letter',
+        url_region)
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=4)
+    lst = [telebot.types.InlineKeyboardButton(
+            alphabet[btn],
+            callback_data='set_region')
+           for btn in range(len(alphabet))]
+    print(lst[0].callback_data)
+    print(lst[0].__str__())
     keyboard.add(*lst)
-    print(lst)
     bot.send_message(
         message.chat.id,
-        get_location(),
+        'Выберите первую букву региона',
         reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def weather_callback(query):
+    data = query.data
+    bot.answer_callback_query(query.id)
+    bot.send_chat_action(query.message.chat.id, 'typing')
+    if data == 'update_current':
+        bot.send_message(
+            query.message.chat.id,
+            get_message(),
+            reply_markup=button(
+                text='Обновить',
+                callback_data='update_current'))
+    elif data == 'update_10_days':
+        bot.send_message(
+            query.message.chat.id,
+            get_message_10_days(),
+            reply_markup=button(
+                text='Обновить',
+                callback_data='update_10_days'))
+    elif data.startswith('set_region'):
+        bot.send_message(
+            query.message.chat.id,
+            'Выберите регион',
+            # set_region(),
+            reply_markup=button(
+                text='pass',
+                callback_data='set_city'))
+    elif data == 'set_city':
+        bot.send_message(
+            query.message.chat.id,
+            get_message_10_days(),
+            reply_markup=button(
+                text='Выберите город',
+                callback_data='set_city'))
+    elif data == 'set_city':
+        pass
+
+
+def set_region(letter):
+    # regions = get_location()
+    pass
 
 
 def get_location():
@@ -176,36 +199,8 @@ def get_location():
     names = [name.get_text() for name in value]
     links = ['https://yandex.ru' +
              link.find('a').get('href') for link in value]
-    regions = list(zip(names, links)) 
-    regions = [': '.join(name) for name in regions]
-    return '\n'.join(regions[:20]) # symbols limit is 4096
-
-
-def gen_row(iterable, num ):
-    s=[]
-    lst=[]
-    #for i in range(len(iterable)):
-    for i in range(19):
-        s.append(i)
-        if len(s) == num:
-            lst.append(s)
-            s=[]
-        elif len(iterable) - (len(lst)*num) < num:
-            lst.append([i])
-    return lst
-
-'''
-def gen_row(iterable, num ):
-    s=[]
-    lst=[]
-    for i in iterable:
-        s.append(i)
-        if len(s) == num:
-            lst.append(s)
-            s=[]
-        elif len(iterable) - (len(lst)*num) < num:
-            lst.append([i])
-    return lst'''
+    regions = dict(zip(names, links))
+    return regions
 
 
 bot.polling(none_stop=True)
