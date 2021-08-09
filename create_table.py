e27204d6855fa7a710c4a5c6cfade4898ca96431
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-08-09 18:45:26 +0600

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -6,7 +6,6 @@ with UseDataBase(config) as cursor:
     cursor.execute("""CREATE TABLE users_property(
                     chat_id INT UNIQUE,
                     url VARCHAR(254),
-                    url_regions VARCHAR(254),
                     url_region VARCHAR(254),
                     PRIMARY KEY (chat_id)
                     );""")
