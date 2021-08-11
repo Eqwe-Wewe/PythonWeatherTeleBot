84bddae83b16dae48dfef6e0d30dbc7576c17dc6
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-08-12 00:24:00 +0600

    minor update

--- users_property.sql
+++ users_property.sql
@@ -1,8 +1,9 @@
 -- таблица для кеширования запросов пользователей
 
-CREATE TABLE IF NOT EXISTS users_property(
-chat_id INT UNIQUE,
-url VARCHAR(254),
-url_region VARCHAR(254),
-PRIMARY KEY (chat_id)
-);
+CREATE TABLE IF NOT EXISTS users_property
+                           (
+                           chat_id INT UNIQUE,
+                           url VARCHAR(254),
+                           url_region VARCHAR(254),
+                           PRIMARY KEY (chat_id)
+                           );
