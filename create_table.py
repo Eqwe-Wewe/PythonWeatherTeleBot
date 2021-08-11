bd34ebd3a3d2db22ed8c24236b27123c41c61a96
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-08-11 23:18:47 +0600

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -2,10 +2,20 @@ from db import UseDataBase
 from config_db import config
 
 
-with UseDataBase(config) as cursor:
-    cursor.execute("""CREATE TABLE users_property(
-                    chat_id INT UNIQUE,
-                    url VARCHAR(254),
-                    url_region VARCHAR(254),
-                    PRIMARY KEY (chat_id)
-                    );""")
+def main():
+    with UseDataBase(config) as cursor:
+        cursor.execute(
+            """
+                CREATE TABLE users_property
+                             (
+                             chat_id INT UNIQUE,
+                             url VARCHAR(254),
+                             url_region VARCHAR(254),
+                             PRIMARY KEY (chat_id)
+                             );
+            """
+        )
+
+
+if __name__ == '__main__':
+    main()
